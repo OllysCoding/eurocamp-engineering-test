04ec5473e1b5d5a3f52ef6220da6275356eee012
Olly Namey <ollyn@hey.com>
2023-05-11 09:16:17 +0100

    chore: remove unused state

--- apps/frontend/src/components/Explorer.tsx
+++ apps/frontend/src/components/Explorer.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react"
 import UserExplorer from './UserExplorer'
 
 const Explorer = () => {
-  const [state, setState] = useState(0)
-
   return <div>
     <h1>Backoffice Data Explorer</h1>
     <p>Welcome to your back-office data explorer, where you can view all of our users private information, without any security checks!</p>
